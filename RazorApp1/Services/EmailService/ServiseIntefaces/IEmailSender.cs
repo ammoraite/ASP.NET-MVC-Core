a7fc01daa068d8c81bc767eb05fcbbc908e20c04
ammoraite <74140087+ammoraite@users.noreply.github.com>
2022-07-25 20:59:38 +0300

    Update README.md

--- RazorApp1/Services/EmailService/ServiseIntefaces/IEmailSender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RazorApp1.Services.EmailService.ServiseIntefaces
-{
-    public interface IEmailSender
-    {
-        public Task SendAsync ( string email, string subject, string message);
-        public Task SendBegetEmailPoliticAsync ( string email, string subject, string message );
-    }
-}
--- RazorApp1/Services/EmailService/SmtpCredentions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RazorApp1.Services.EmailService
-{
-    public class SmtpCredentions
-    {
-        public string Host { get; set; } = "";
-        public string UserName { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
-}
